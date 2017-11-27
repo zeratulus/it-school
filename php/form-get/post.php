c5db8a0236acbdc30cc0e939b747c81fc3cfbb83
ailus665 <ailus665@gmail.com>
2017-11-27 18:07:40 +0200

    Added lessons.

--- php/form-get/post.php
+++ php/form-get/post.php
@@ -32,3 +32,14 @@ if ($pass != $pass_confirm) {
 if (!$agreement) {
     echo 'Error: You should check User Agreement<br>';
 }
+
+$get_url = '';
+
+$i = 0;
+foreach ($_POST as $key => $value) {
+    if ($i > 0) $get_url .= '&';
+    $get_url .= $key . '=' . $value;
+    $i++;
+}
+$url = 'http://google.com.ua/index.php';
+echo $url . '?' . $get_url;
