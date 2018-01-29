666830c97211b09e1b397698e2b9acad0c099b96
ailus665 <ailus665@gmail.com>
2018-01-29 18:49:41 +0200

    Battle Process Update DB class update

--- php/oop/step-game/battle.php
+++ php/oop/step-game/battle.php
@@ -6,6 +6,11 @@
  * Time: 18:14
  */
 
+//SOME SQL EXAMPLES:
+//INSERT INTO battles(id, player_id, p_hp, p_ap, p_mp, e_hp, e_ap, e_mp, enemy_id) VALUES (1, 1, 100, 100, 100, 75, 75, 75, 1)
+//
+//
+
 ini_set('display_errors', 1);
 
 require_once 'config.php';
@@ -19,12 +24,23 @@ $request = new Request();
 
 $db = new DB(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DB);
 
-$player = new GameObjects\BattleUnit(100, 100, 100, 3, 5, 'img/cat.jpg', 'I am Player', 5, 5);
-$enemy = new GameObjects\BattleUnit(100, 100, 100, 1, 5, 'img/slim.jpg', 'Slim', 5, 5);
+$battle = $db->query('SELECT * FROM battles WHERE id = 1 LIMIT 1')->row;
+
+$player = new GameObjects\BattleUnit($battle['p_hp'], $battle['p_mp'], $battle['p_ap'], 3, 5, 'img/cat.jpg', 'I am Player', 5, 5);
+//$player = new GameObjects\BattleUnit(100, 100, 100, 3, 5, 'img/cat.jpg', 'I am Player', 5, 5);
+$enemy = new GameObjects\BattleUnit($battle['e_hp'], $battle['e_mp'], $battle['e_ap'], 1, 5, 'img/slim.jpg', 'Slim', 5, 5);
+//$enemy = new GameObjects\BattleUnit(100, 100, 100, 1, 5, 'img/slim.jpg', 'Slim', 5, 5);
 
 if ($request->server['REQUEST_METHOD'] == 'POST') {
     $player->makeDamage($enemy);
     $enemy->makeDamage($player);
+
+    if ($enemy->hp > 0) {
+        $db->query('UPDATE battles SET player_id=1, p_hp='.$player->hp.', p_ap='.$player->ap.', p_mp='.$player->mp.', e_hp='.$enemy->hp.', e_ap='.$enemy->ap.', e_mp='.$enemy->mp.', enemy_id=1 WHERE id=1');
+    } else {
+        // End of battle, player earn XP by enemy_id
+        $db->query('DELETE FROM battles WHERE id = ' . $battle['id']);
+    }
 }
 
 ?>
@@ -41,23 +57,31 @@ if ($request->server['REQUEST_METHOD'] == 'POST') {
 
 <form action="battle.php" method="post">
 
-    <div class="battle-flex">
-        <div class="battle-panel left">
-            <h2 class="text-center"><?php echo $player->name; ?></h2>
-            <img src="<?php echo $player->image; ?>" class="player">
-            <p><?php showUnitParams($player); ?></p>
+    <?php if ($enemy->hp > 0) { ?>
+        <div class="battle-flex">
+            <div class="battle-panel left">
+                <h2 class="text-center"><?php echo $player->name; ?></h2>
+                <img src="<?php echo $player->image; ?>" class="player">
+                <p><?php showUnitParams($player); ?></p>
+            </div>
+            <div class="battle-panel right">
+                <h2 class="text-center"><?php echo $enemy->name; ?></h2>
+                <img src="<?php echo $enemy->image; ?>" class="enemy">
+                <p><?php showUnitParams($enemy); ?></p>
+            </div>
         </div>
-        <div class="battle-panel right">
-            <h2 class="text-center"><?php echo $enemy->name; ?></h2>
+
+        <div class="flex-center">
+            <input type="submit" value="Attack!" class="btn btn-primary">
+        </div>
+    <?php } else { ?>
+        <div class="flex-center flex-wrap">
+            <h1>You win this <?php echo $enemy->name; ?></h1>
+        </div>
+        <div class="flex-center flex-wrap">
             <img src="<?php echo $enemy->image; ?>" class="enemy">
-            <p><?php showUnitParams($enemy); ?></p>
         </div>
-    </div>
-
-    <div class="flex-center">
-        <input type="submit" value="Attack!" class="btn btn-primary">
-    </div>
-
+    <?php } ?>
 </form>
 
 </body>
--- php/oop/step-game/system/db.php
+++ php/oop/step-game/system/db.php
@@ -8,22 +8,38 @@
 class DB {
     private $link;
 
-    public function __construct($host, $port, $user, $pass, $db)
-    {
+    public function __construct($host, $port, $user, $pass, $db) {
         $this->link = new mysqli($host, $user, $pass, $db, $port);
         $this->link->set_charset('utf8');
     }
 
+    public function __destruct() {
+        $this->link->close();
+    }
+
     public function query($sql) {
-        $results = $this->link->query($sql);
+        $query = $this->link->query($sql);
 
-        $data = array();
+        if (!$this->link->errno) {
+            if ($query instanceof \mysqli_result) {
+                $data = array();
 
-        while ($row = $results->fetch_assoc()) {
-            $data[] = $row;
+                while ($row = $query->fetch_assoc()) {
+                    $data[] = $row;
+                }
+
+                $result = new \stdClass();
+                $result->num_rows = $query->num_rows;
+                $result->row = isset($data[0]) ? $data[0] : array();
+                $result->rows = $data;
+
+                $query->close();
+
+                return $result;
+            } else {
+                return true;
+            }
         }
-
-        return $data;
     }
 
 }
